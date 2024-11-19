4c1d3d471fc515a8a6477275da051fd6afcb15bc
HirokiMiyakura <miyakurahiroki@gmail.com>
2024-11-19 17:24:00 +0900

    5th commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
+import Link from 'next/link'; // Linkをインポート
 import styles from './Header.module.css';
 import MenuList from './MenuList';
 
@@ -29,9 +30,9 @@ export const Header = () => {
 							で人工芝を施工するなら
 						</span>
 						<h1 className="text-2xl sm:text-4xl font-extrabold leading-none">
-							<a className="hover:opacity-85" href="/">
+							<Link className="hover:opacity-85" href="/">
 								<span className="text-green-600">緑</span>の達人
-							</a>
+							</Link>
 						</h1>
 					</div>
 
@@ -47,83 +48,83 @@ export const Header = () => {
 
 					<ul className="hidden sm:flex gap-x-4">
 						<li className="text-3xl font-extrabold text-gray-800">
-							<a href="">
+							<Link href="">
 								☎︎ 080-3438-3925
 								<br />
 								<span className="text-sm block text-center text-gray-800">
 									9時~17時（土日祝日除く）
 								</span>
-							</a>
+							</Link>
 						</li>
 					</ul>
 				</div>
 				<div className="hidden sm:block bg-green-600 text-white">
 					<ul className="flex justify-between max-w-screen-xl mx-auto gap-x-10">
 						<li>
-							<a
+							<Link
 								className="tracking-wider py-7 block hover:opacity-85"
 								href="#ABOUT"
 							>
 								緑の達人とは
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								className="tracking-wider py-7 block hover:opacity-85"
 								href="#FLOW"
 							>
 								サービスの流れ
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								className="tracking-wider py-7 block hover:opacity-85"
 								href="#SAMPLES"
 							>
 								施工事例
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								className="tracking-wider py-7 block hover:opacity-85"
 								href="#NEWS"
 							>
 								新着記事
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								className="tracking-wider py-7 block hover:opacity-85"
 								href="#VOICE"
 							>
 								お客様の声
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								className="tracking-wider py-7 block hover:opacity-85"
 								href="#FAQ"
 							>
 								よくあるご質問
-							</a>
+							</Link>
 						</li>
 						<li>
 							<ul className="flex">
 								<li className="bg-yellow-500">
-									<a
+									<Link
 										className="tracking-widest px-10 py-7 block hover:opacity-85"
 										href="#ESTIMATE"
 									>
 										簡単お見積り
-									</a>
+									</Link>
 								</li>
 								<li className="bg-orange-600">
-									<a
+									<Link
 										className="tracking-widest px-10 py-7 block hover:opacity-85"
 										href="#CONTACT"
 									>
 										お問い合わせ
-									</a>
+									</Link>
 								</li>
 							</ul>
 						</li>
